b92a09aae32210e65cf9481f7e6597b8cd5fdac7
mirthbottle <mirthbottle@gmail.com>
2023-06-22 13:05:06 -0700

    add init.el
    configuring emacs

---
+++ init.el
@@ -0,0 +1,52 @@
+;;; emacs init file inside .emacs.d/
+;;; package --- interactive mode
+;;; Commentary:
+;;; Code:
+(require 'ido)
+(ido-mode t)
+
+(setq column-number-mode t)
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(require 'use-package)
+
+(ac-config-default)
+
+(global-auto-complete-mode t)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+;;(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(package-selected-packages '(use-package flycheck)))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; )
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(provide 'init)
+;;; init.el ends here
