0bae660fdb3d8b1588a34759239a3daa904d0c08
mirthbottle <mirthbottle@gmail.com>
2023-07-30 22:45:53 -0700

    save files before refreshing line endings

--- float_test.py
+++ float_test.py
@@ -6,14 +6,14 @@ from decimal import Decimal, getcontext
 f = 1.2 - 1
 f != 0.2
 
-f = sum([float(0.1)]*10)
+f = sum([float(0.1)] * 10)
 f != 1
 
-math.fsum([0.1]*10) == 1
+math.fsum([0.1] * 10) == 1
 
-cProfile.run("sum([Decimal('0.1')]*1000)")
+# cProfile.run("sum([Decimal('0.1')]*1000)")
 
 with cProfile.Profile() as pr:
-    sum([0.1]*10)
+    sum([0.1] * 10)
 
     pr.print_stats()
