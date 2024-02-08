64e369df22e15439e73d6869460c614bc0c141a9
mirthbottle <mirthbottle@gmail.com>
2024-02-08 15:33:36 -0800

    move assets to assets.positions file

--- projects/gain_tracker/gain_tracker/definitions.py
+++ projects/gain_tracker/gain_tracker/definitions.py
@@ -8,53 +8,14 @@ import os
 import pandas as pd
 
 from dagster_gcp_pandas import BigQueryPandasIOManager
-from dagster import Definitions, SourceAsset, asset
-
-import yfinance as yf
+from dagster import Definitions, asset
 
 from resources.etrade_resource import ETrader
 
-# from assets.positions import etrade_positions
+from assets.positions import (
+    positions_data, etrade_accounts, etrade_positions,
+    benchmark_history)
 
-DEFAULT_BENCHMARK_TICKER="IVV"
-
-# name is wrong
-positions_data = SourceAsset(key="positions_dec")
-
-@asset
-def etrade_accounts(etrader: ETrader):
-    """Pull accounts in etrade
-
-    see if dagster can trigger opening a website and have a user input
-    """
-    accounts = etrader.list_accounts()
-    return pd.DataFrame(accounts)
-
-@asset
-def etrade_positions(etrader: ETrader, etrade_accounts: pd.DataFrame):
-    """Pull positions in etrade for each account
-    """
-    keys = etrade_accounts["accountidkey"].values
-
-    all_positions = []
-    for k in keys:
-        portfolio = etrader.view_portfolio(k)
-        if portfolio is not None:
-            ps = pd.DataFrame(portfolio)
-            ps.loc[:, "account_id_key"] = k
-            all_positions.append(ps)
-
-    positions = pd.concat(all_positions)
-    return positions
-
-@asset
-def benchmark_history(positions_dec:pd.DataFrame):
-    """Pull benchmark prices, dummy asset
-    """
-    earliest_date = positions_dec["position_entry_date"].min()
-    bm = yf.Ticker(DEFAULT_BENCHMARK_TICKER)
-    bm_hist = bm.history(start=earliest_date)
-    return bm_hist.reset_index()
 
 @asset
 def positions_count(
